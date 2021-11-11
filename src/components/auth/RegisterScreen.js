86917316fb6a14f6bef17aa6ab01f5b407e2b230
Rogelio Llerena <rogelio.llerena@ceiba.com.co>
2021-11-11 18:08:06 -0500

    Agregando 'Validator', para el formulario de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -2,29 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "../../hooks/useForm";
 
+import validator from 'validator';
+
 export default function RegisterScreen() {
 
   const initialForm = {
     name: 'Rogelio',
     email: 'rogelio@correo.com',
-    password: '12345',
-    password2: '12345',
+    password: '123456',
+    password2: '123456',
 };
   const [ formValues, handleInputChange] = useForm( initialForm );
   const {name, email, password, password2 } = formValues;
 
   const handleRegistrer = (e) => {
       e.preventDefault();
+
+      if (isFormValid()){
+        console.log('Formulario correcto');
+      }
   }
 
   const isFormValid = () =>{
-      //TODO:
+      if(name.trim().length===0){
+        console.log('Name is required');
+        return false;
+      }else if( !validator.isEmail(email) ){
+        console.log('Email is Not valid');
+        return false;
+      }else if (password !== password2 || password.length <= 6){
+        console.log('Password should be at last 6 characters and match each other');
+        return false;
+      }
+
+      return true;
   }
 
   return (
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={ handleRegistrer }>
+
+        <div className="auth__alert-error">
+            Hola Mundo
+        </div>
+
          <input
           type="text"
           placeholder="Name"
